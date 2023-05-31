988334bd4c926704745f710914caf621106fb57a
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-06-01 01:17:34 +0300

    Fixed bugs

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,11 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().allMatch(x -> Integer.parseInt(x) > 0);
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
